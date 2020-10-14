42dc1ba556731a9dc1d6eef1978fe0f2439fd035
TlouerElle <61647484+TlouerElle@users.noreply.github.com>
2020-10-14 08:34:06 +0800

    等腰三角形
    等腰三角形

---
+++ sy24.java
@@ -0,0 +1,23 @@
+package sy24;
+
+import java.util.Scanner;
+
+public class sy24 {
+	public static void main(String[] args) {
+		int i,j,k,n;
+		Scanner reader=new Scanner(System.in);
+		System.out.print("«Î ‰»În=");
+		n=reader.nextInt();
+		for(i=1;i<=n;i++) {
+			for(k=1;k<=n-i;k++) {
+				System.out.print(" ");
+			}
+			for(j=1;j<=2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
+
