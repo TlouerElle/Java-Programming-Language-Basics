a2879d66fcd6206fc450956fd8868982fb06ad26
TlouerElle <61647484+TlouerElle@users.noreply.github.com>
2020-10-14 08:36:40 +0800

    求数列的前n项和(1×2×3)+(3×4×5)+(5×6×7)+...
    求数列的前n项和(1×2×3)+(3×4×5)+(5×6×7)+...

---
+++ sy26.java
@@ -0,0 +1,25 @@
+package sy;
+
+import java.util.Scanner;
+
+public class sy {
+	public static void main(String[] args) {
+		int i,j=0,n;
+		Scanner reader=new Scanner(System.in);
+		System.out.print("请输入整数n=");
+		n=reader.nextInt();
+		if(n%2!=0) {
+			for(i=1;i<n;i=i+2) {
+				j=j+i*(i+1)*(i+2);
+			}
+			System.out.printf("结果为：%d",j);
+		}
+		if(n%2==0) {
+			for(i=1;i<n-1;i=i+2) {
+				j=j+i*(i+1)*(i+2);
+				j=j+(n-1)*n;
+			}
+			System.out.printf("结果为：%d",j);
+		}
+	}
+}
