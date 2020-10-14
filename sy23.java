71499b217a480b153d9311139cb6570e75a7309b
TlouerElle <61647484+TlouerElle@users.noreply.github.com>
2020-10-14 08:32:32 +0800

    1加到1000
    1加到1000

---
+++ sy23.java
@@ -0,0 +1,12 @@
+package sy23;
+
+public class sy23 {
+	public static void main(String[] args) {
+		int i,sum=0;
+		for(i=1;i<=1000;i++) {
+			sum=sum+i;
+		}
+		System.out.printf("1+2+...+1000=%d",sum);
+	}
+
+}
